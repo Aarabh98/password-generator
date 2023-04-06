6272158c1e05ecd5fe315ebf67fc65b9759f1e8d
sourabh kushwah <skushwah1420@gmail.com>
2023-04-06 12:33:43 +0530

    create password with generator
    password generator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import PasswordGenerator from "./Component/PasswordGenerator";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <div className="App">
+      <PasswordGenerator />
+    </div>
+  );
+}
+
+export default App;
